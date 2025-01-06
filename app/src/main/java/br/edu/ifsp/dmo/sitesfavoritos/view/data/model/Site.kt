bb7c0f0d295442b8000e23d72a66eeba92e11a62
Felipe <fepaganini6@gmail.com>
2025-01-06 01:16:28 -0300

    MVVM inacabado

---
+++ app/src/main/java/br/edu/ifsp/dmo/sitesfavoritos/view/data/model/Site.kt
@@ -0,0 +1,5 @@
+package br.edu.ifsp.dmo.sitesfavoritos.view.data.model
+
+class Site(var apelido: String, var url: String) {
+    var favorito: Boolean = false
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo/sitesfavoritos/view/ui/listeners/SiteItemClickListener.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.dmo.sitesfavoritos.view.ui.listeners
+
+interface SiteItemClickListener {
+    fun clickSiteItem(position: Int)
+    fun clickHeartSiteItem(position: Int)
+}
+
